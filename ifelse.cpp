7773321fc3b61e4fe8d53b742a5ca87ea1d61b62
Saurabhdwivedi264 <72238889+Saurabhdwivedi264@users.noreply.github.com>
2023-03-30 00:15:06 +0530

    Add files via upload

---
+++ ifelse.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int savings;
+    cin>>savings;
+    if(savings>5000){
+        if{savings>1000}
+        cout<<"Roadtrip with neha\n";
+    }
+    else {
+        cout<<"Shoping with neha\n";
+    }
+    } else if (savings>2000){
+        cout<<"Rashmi\n";
+    }
+    else {
+        cout<<"friends\n";
+    }
+    return 0;
+}
